93aeaf6bd912874e17d285b800343971b1c00a50
ahn-sj <jaesa5221@gmail.com>
2025-07-17 19:28:08 +0900

    first commit

---
+++ monolitic/src/main/java/tally/monolitic/MonoliticApplication.java
@@ -0,0 +1,13 @@
+package tally.monolitic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoliticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonoliticApplication.class, args);
+    }
+
+}
---
+++ monolitic/src/test/java/tally/monolitic/MonoliticApplicationTests.java
@@ -0,0 +1,13 @@
+package tally.monolitic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoliticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
