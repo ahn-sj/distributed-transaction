7e50deab9e7800cf424ea71a6ff18a22255496d1
ahn-sj <jaesa5221@gmail.com>
2025-07-18 13:18:43 +0900

    add initial project structure with Gradle configuration and application classes

---
+++ point/src/test/java/tally/point/PointApplicationTests.java
@@ -0,0 +1,13 @@
+package tally.point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
